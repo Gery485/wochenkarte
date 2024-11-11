e2eb6cbe9888eea3142e1da727b121227617e658
Sophie <n.gerdom@gmail.com>
2024-11-11 22:06:39 +0100

    simpilfy

--- logout.php
+++ logout.php
@@ -1,12 +1,6 @@
 <?php
 session_start();
-require_once 'classes/CookieHelper.php';
 require_once 'classes/User.php';
-
-if (!CookieHelper::isCookieSet('allowed')) {
-    header("Location: index.php");
-    exit();
-}
 
 User::logout();
 header("Location: index.php");
