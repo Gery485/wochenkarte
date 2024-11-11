31fd8e812b653c9b04c9a617f39fbaf5fdd6e565
Sophie <n.gerdom@gmail.com>
2024-11-11 22:05:45 +0100

    design

--- wochenkarte.php
+++ wochenkarte.php
@@ -15,13 +15,53 @@ if (!CookieHelper::isCookieSet('allowed') || !User::isLoggedIn()) {
     <meta charset="UTF-8">
     <title>Wochenkarte</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        /* Styling für die Menüanzeige */
+        .menu-grid {
+            display: grid;
+            grid-template-columns: repeat(3, 1fr);
+            gap: 20px;
+        }
+        .menu-item {
+            text-align: center;
+        }
+        .menu-item img {
+            width: 100%;
+            height: auto;
+            border-radius: 5px;
+        }
+    </style>
 </head>
 <body class="container">
-    <h1>Aktuelle Tagesmenüs</h1>
-    <!-- Hier können Tagesmenüs für die einzelnen Wochentage angezeigt werden -->
-    <p>Montag: Rührei mit Speck</p>
-    <p>Dienstag: Pfannkuchen</p>
-    <p>...</p>
-    <a href="logout.php" class="btn btn-danger">Logout</a>
+    <h1 class="text-center mt-5">Wochenkarte</h1>
+
+    <div class="menu-grid mt-5">
+        <!-- Bilder und Beschriftungen für jeden Tag -->
+        <div class="menu-item">
+            <img src="path/to/montag.jpg" alt="Montag">
+            <p>Montag</p>
+        </div>
+        <div class="menu-item">
+            <img src="path/to/dienstag.jpg" alt="Dienstag">
+            <p>Dienstag</p>
+        </div>
+        <div class="menu-item">
+            <img src="path/to/mittwoch.jpg" alt="Mittwoch">
+            <p>Mittwoch</p>
+        </div>
+        <div class="menu-item">
+            <img src="path/to/donnerstag.jpg" alt="Donnerstag">
+            <p>Donnerstag</p>
+        </div>
+        <div class="menu-item">
+            <img src="path/to/freitag.jpg" alt="Freitag">
+            <p>Freitag</p>
+        </div>
+        <div class="menu-item">
+            <p>Samstag<br>Ruhetag</p>
+        </div>
+    </div>
+    
+    <a href="logout.php" class="btn btn-link mt-4">Logout</a>
 </body>
 </html>
